ff1d8d3fd5a7c0a3ff9d6b8b0fe5a6cb6c424029
Andrew Dassonville <dassonville.andrew@gmail.com>
2017-09-19 12:04:19 -0700

    Format lines to correct length

--- src/test/java/org/sert2521/gamename/Utils.kt
+++ src/test/java/org/sert2521/gamename/Utils.kt
@@ -4,7 +4,9 @@ import org.fest.assertions.Assertions.assertThat
 import org.strongback.components.Motor
 import org.strongback.components.ui.ContinuousRange
 
-fun assertMotorSpeeds(speed: Double, motors: List<Motor>, squareSpeed: Boolean = false) = motors.forEach {
+fun assertMotorSpeeds(speed: Double,
+                      motors: List<Motor>,
+                      squareSpeed: Boolean = false) = motors.forEach {
     val expectedSpeed = if (squareSpeed) Math.pow(speed, 2.0) else speed
     assertThat(it.speed).isEqualTo(expectedSpeed)
 }
