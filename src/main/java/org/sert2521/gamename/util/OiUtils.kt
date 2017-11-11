eb6b34b3130f14b6ab687a5b53f65021cffa15eb
Alex Saveau <saveau.alexandre@gmail.com>
2017-11-10 17:21:37 -0800

    Fix inverted throttle
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- src/main/java/org/sert2521/gamename/util/OiUtils.kt
+++ src/main/java/org/sert2521/gamename/util/OiUtils.kt
@@ -8,8 +8,8 @@ import org.strongback.hardware.Hardware
 val leftJoystick: FlightStick = Hardware.HumanInterfaceDevices.logitechAttack3D(LEFT_STICK_PORT)
 val rightJoystick: FlightStick = Hardware.HumanInterfaceDevices.logitechAttack3D(RIGHT_STICK_PORT)
 
-val FlightStick.scaledThrottle
-    get() = throttle.map { (it + 1) * .5 }
+val FlightStick.scaledThrottle: ContinuousRange
+    get() = throttle.invert().map { (it + 1) * .5 }
 val FlightStick.scaledPitch: ContinuousRange
     get() = pitch.scale(Preferences.getInstance().getDouble("scaled_speed", 1.0))
 val FlightStick.scaledRoll: ContinuousRange
