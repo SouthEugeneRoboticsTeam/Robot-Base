a7d52859133ab88bcc9ad3a5e3d2287872fe60b0
Alex Saveau <saveau.alexandre@gmail.com>
2018-01-26 16:37:21 -0800

    Cleanup
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- src/main/java/org/sert2521/gamename/drivetrain/Drivetrain.kt
+++ src/main/java/org/sert2521/gamename/drivetrain/Drivetrain.kt
@@ -6,7 +6,6 @@ import org.sert2521.gamename.util.LEFT_REAR_MOTOR
 import org.sert2521.gamename.util.RIGHT_FRONT_MOTOR
 import org.sert2521.gamename.util.RIGHT_REAR_MOTOR
 import org.sert2521.gamename.util.leftJoystick
-import org.sertain.RobotLifecycle
 import org.sertain.command.Subsystem
 import org.sertain.hardware.Talon
 import org.sertain.hardware.autoBreak
@@ -14,7 +13,7 @@ import org.sertain.hardware.inverted
 import org.sertain.hardware.plus
 import org.sertain.hardware.resetEncoder
 
-object Drivetrain : Subsystem(), RobotLifecycle {
+object Drivetrain : Subsystem() {
     private val frontLeft = Talon(LEFT_FRONT_MOTOR) + Talon(LEFT_REAR_MOTOR)
     private val frontRight =
             Talon(RIGHT_FRONT_MOTOR).inverted() + Talon(RIGHT_REAR_MOTOR).inverted()
